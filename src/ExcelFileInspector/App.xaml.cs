5ad23f32e55636cbcb6ec48bd853ab06918a51cc
overdrive1708 <52090022+overdrive1708@users.noreply.github.com>
2024-11-02 14:45:54 +0900

    Create project files.

---
+++ src/ExcelFileInspector/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using ExcelFileInspector.Utilities;
+using ExcelFileInspector.Views;
+using Prism.Ioc;
+using System;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ExcelFileInspector
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+
+        /// <summary>
+        /// Startup処理
+        /// </summary>
+        /// <param name="sender">イベントソース</param>
+        /// <param name="e">イベントデータ</param>
+        private void PrismApplication_Startup(object sender, StartupEventArgs e)
+        {
+            // 未処理の例外が発生したときの処理を登録する｡
+            DispatcherUnhandledException += ExceptionHandler.OnDispatcherUnhandledException;
+            TaskScheduler.UnobservedTaskException += ExceptionHandler.OnUnobservedTaskException;
+            AppDomain.CurrentDomain.UnhandledException += ExceptionHandler.OnUnhandledException;
+        }
+    }
+}
